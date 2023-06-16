20c2fdafc33d8e92d225cce3c1c57d9db715f12a
Natan Foleto <natanfoleto2015@hotmail.com>
2023-06-16 01:14:21 -0300

    Finished challenge!

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# devs-api
-DevsAPI
+# DevsAPI
+
+---
+
+### Requisitos
+
+- [] Deve ser possível criar um dev
+- [] Deve ser possível buscar todos devs
+- [] Deve ser possível buscar um dev pelo username
+
+---
+
+### Regras de negócio
+
+- [] Não deve ser possível cadastrar um dev com mesmo username
+- [] Não deve ser possível criar um dev que não tenha conta no Github
+- [] Não deve ser possível criar um dev sem que o CEP passado seja válido
+- [] Na listagem por username, deverá vir o cadastro do dev e mais informações do seu Github
--- src/app.js
+++ src/app.js
@@ -1,59 +1,102 @@
-const express = require('express')
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const devs = []
+const devs = [];
 
-const GITHUB_URL = "https://api.github.com/users"
+const GITHUB_URL = "https://api.github.com/users";
+const VIACEP_URL = "https://viacep.com.br/ws";
 
 async function getUserFromGithub(username) {
-  const { data } = await axios.get(`${GITHUB_URL}/${username}`)
+  try {
+    const { data } = await axios.get(`${GITHUB_URL}/${username}`);
 
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
 }
 
-app.post('/devs', async (req, res) => {
+async function getAddressByCep(cep) {
   try {
-    const { username } = req.body
+    const { data } = await axios.get(`${VIACEP_URL}/${cep}/json`);
 
-    const devAlreadyExists = devs.some(dev => dev.username === username);
-
-    if (devAlreadyExists) {
-      return res.status(400).json({ message: "Já existe um dev com esse usuário!" })
-    }
-
-    const user = await getUserFromGithub(username)
-
-    devs.push({
-      id: user.id,
-      name: user.name,
-      username
-    })
-
-    return res.status(201).json({ 
-      message: "Dev criado com sucesso!" 
-    })
+    return data;
   } catch (error) {
-    return res.status(400).json({ 
-      message: error.response.data.message 
-    })
+    console.log(error.response.data);
   }
-})
+}
 
-app.get('/devs', (req, res) => {
-  return res.json(devs)
-})
+app.post("/devs", async (req, res) => {
+  const { username, cep } = req.body;
 
-app.get('/devs/:username', async (req, res) => {
-  try {
-    const { username } = req.params;
+  const devAlreadyExists = devs.some((dev) => dev.username === username);
 
-    const user = await getUserFromGithub(username)
+  if (devAlreadyExists) {
+    return res
+      .status(400)
+      .json({ message: "Já existe um dev com esse username!" });
+  }
 
-    return res.json({
+  const user = await getUserFromGithub(username);
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Usuário não encontrado no Github!" });
+  }
+
+  const address = await getAddressByCep(cep);
+
+  if (!address) {
+    return res.status(400).json({ message: "Por favor insira um CEP válido!" });
+  }
+
+  const dev = {
+    id: user.id,
+    name: user.name,
+    username,
+    address: {
+      cep: address.cep,
+      street: address.logradouro,
+      complement: address.complemento,
+      neighborhood: address.bairro,
+      city: address.cidade,
+      state: address.uf,
+    },
+  };
+
+  devs.push(dev);
+
+  return res.status(201).json({
+    message: "Dev criado com sucesso!",
+    dev,
+  });
+});
+
+app.get("/devs", (req, res) => {
+  return res.json(devs);
+});
+
+app.get("/devs/:username", async (req, res) => {
+  const { username } = req.params;
+
+  const dev = devs.find((dev) => dev.username === username);
+
+  if (!dev) {
+    return res
+      .status(400)
+      .json({ message: "Nenhum dev encontrado com esse username!" });
+  }
+
+  const user = await getUserFromGithub(username);
+
+  return res.json({
+    dev,
+    github: {
       id: user.id,
       name: user.name,
       username: user.login,
@@ -69,12 +112,8 @@ app.get('/devs/:username', async (req, res) => {
       followers: user.followers,
       following: user.following,
       createdAt: user.created_at,
-    })
-  } catch (error) {
-    return res.status(400).json({ 
-      message: error.response.data.message 
-    })
-  }
-})
+    },
+  });
+});
 
-app.listen(3333)
+app.listen(3333);
